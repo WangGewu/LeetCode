9d993ed133577f909e265e76e8050619cdebf1dc
Gewu_Wang <35912578+WangGewu@users.noreply.github.com>
2018-03-23 09:30:36 +0800

    Add files via upload

---
+++ LeetCode/028. Implement strStr().cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int len = needle.length();
+        if (len>haystack.length())
+            return -1;
+        for (int i = 0; i<haystack.length() - len + 1; i++)
+            if (haystack.substr(i, len) == needle)
+                return i;
+        return -1;
+    }
+};
