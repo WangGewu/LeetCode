56257ac5b915f2301557bdb4599b0dd6e55b1573
Gewu_Wang <35912578+WangGewu@users.noreply.github.com>
2018-04-02 13:54:05 +0800

    Update 048. Rotate Image.cpp

--- LeetCode/048. Rotate Image.cpp
+++ LeetCode/048. Rotate Image.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& matrix) {
-        //·­×ªÔÙ×ªÖÃ
+        //å…ˆç¿»è½¬ï¼Œå†è½¬ç½®
         reverse(matrix.begin(), matrix.end());
         for (int i = 0; i < matrix.size(); ++i)
             for (int j = i + 1; j < matrix[0].size(); ++j)
