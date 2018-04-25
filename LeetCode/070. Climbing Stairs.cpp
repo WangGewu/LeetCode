c37b53f17a8665ad97f3e3a424d97ee5d03a6795
Gewu_Wang <35912578+WangGewu@users.noreply.github.com>
2018-04-25 14:01:27 +0800

    Add files via upload

---
+++ LeetCode/070. Climbing Stairs.cpp
@@ -0,0 +1,16 @@
+//¶¯Ì¬¹æ»®
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int>res{ 1,2 };
+        int last1 = 1;
+        int last2 = 2;
+        for (int i = 2; i < n; i++)
+        {
+            res.push_back(last1 + last2);
+            last1 = last2;
+            last2 = res[i];
+        }
+        return res[n - 1];
+    }
+};
