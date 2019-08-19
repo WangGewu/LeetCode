64bc1f72c736f02237af082345cf2b4a396764d6
Gewu_Wang <35912578+WangGewu@users.noreply.github.com>
2019-08-19 18:51:27 +0800

    Update 002. Add Two Numbers.cpp

--- LeetCode/002. Add Two Numbers.cpp
+++ LeetCode/002. Add Two Numbers.cpp
@@ -1,58 +1,57 @@
 /**
-* Definition for singly-linked list.
-* struct ListNode {
-*     int val;
-*     ListNode *next;
-*     ListNode(int x) : val(x), next(NULL) {}
-* };
-*/
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
-	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-		int flag = 0;//记录进位
-		int k = (l1->val + l2->val) % 10;
-		if (l1->val + l2->val >= 10)flag = 1;
-		ListNode *head = new ListNode(k);
-		ListNode *front = head;
-		ListNode *p = l1->next, *q = l2->next;
-		while (p&&q)
-		{
-			ListNode *temp = new ListNode((p->val + q->val + flag) % 10);
-			front->next = temp;
-			front = temp;
-			if (p->val + q->val + flag >= 10)
-				flag = 1;
-			else
-				flag = 0;
-			p = p->next; q = q->next;
-		}
-		while (p)
-		{
-			ListNode *temp = new ListNode((p->val + flag) % 10);
-			front->next = temp;
-			front = temp;
-			if (p->val + flag >= 10)
-				flag = 1;
-			else
-				flag = 0;
-			p = p->next;
-		}
-		while (q)
-		{
-			ListNode *temp = new ListNode((q->val + flag) % 10);
-			front->next = temp;
-			front = temp;
-			if (q->val + flag >= 10)
-				flag = 1;
-			else
-				flag = 0;
-			q = q->next;
-		}
-		if (flag)//最后一位处理后还有进位
-		{
-			ListNode *temp = new ListNode(1);
-			front->next = temp;
-		}
-		return head;
-	}
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int flag=0;
+        ListNode *head=new ListNode(0);
+        ListNode *current_p=head;
+        ListNode *new_value;
+        while(l1&&l2)
+        {
+            int value=(l1->val+l2->val+flag)%10;
+            if(l1->val+l2->val+flag>=10)
+                flag=1;
+            else
+                flag=0;
+            new_value=new ListNode(value);
+            current_p->next=new_value;
+            current_p=new_value;
+            l1=l1->next;l2=l2->next;
+        }
+        while(l1)
+        {
+            new_value=new ListNode((flag+l1->val)%10);
+            if(flag+l1->val>=10)
+                flag=1;
+            else
+                flag=0;
+            current_p->next=new_value;
+            current_p=new_value;
+            l1=l1->next;
+        }
+        while(l2)
+        {
+            new_value=new ListNode((flag+l2->val)%10);
+            if(flag+l2->val>=10)
+                flag=1;
+            else
+                flag=0;
+            current_p->next=new_value;
+            current_p=new_value;
+            l2=l2->next;
+        }
+        if(flag==1)
+        {
+            new_value=new ListNode(1);
+            current_p->next=new_value;
+        }
+        return  head->next;
+    }
 };
