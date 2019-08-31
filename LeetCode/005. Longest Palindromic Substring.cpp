7ed67488a9accd1c8d4e48708be8f74613f448f4
Gewu_Wang <35912578+WangGewu@users.noreply.github.com>
2019-08-31 21:51:01 +0800

    Update 005. Longest Palindromic Substring.cpp

--- LeetCode/005. Longest Palindromic Substring.cpp
+++ LeetCode/005. Longest Palindromic Substring.cpp
@@ -1,40 +1,43 @@
 class Solution {
 public:
-	string longestPalindrome(string s) {
-		string result;
-		int cnt = 0, max = 0;
-		int i, j;
-		for (i = 0; i < s.size(); i++)
-		{
-			//第一种情况（bb）
-			cnt = 0;
-			for (j = 0; (i + j) < s.size() && (i - j) >= 0; j++)
-			{
-				if (s[i - j] == s[i + 1 + j])
-					cnt += 2;
-				else
-					break;
-			}
-			if (cnt > max)
-			{
-				max = cnt;
-				result.assign(s.begin() + i - j + 1, s.begin() + i + j + 1);
-			}
-			//第二种情况（bab）
-			cnt = 1;
-			for (j = 1; (i + j) < s.size() && (i - j) >= 0; j++)
-			{
-				if (s[i - j] == s[i + j])
-					cnt += 2;
-				else
-					break;
-			}
-			if (cnt > max)
-			{
-				max = cnt;
-				result.assign(s.begin() + i - j + 1, s.begin() + i + j);
-			}
-		}
-		return result;
-	}
+    string longestPalindrome(string s) {
+        string result;
+        int first=0,max=0;
+        int cnt;
+        for(int i=0;i<s.length();i++)
+        {
+            //'bab'
+            cnt=1;
+            for(int j=1;(j+i)<s.length()&&(i-j)>=0;j++)
+            {
+                if(s[i+j]==s[i-j])
+                    cnt+=2;
+                else
+                    break;
+            }
+            if(cnt>max)
+            {
+                max=cnt;
+                result=s.substr(i-cnt/2,cnt);
+            }
+            //'bb'
+            if(i>=1&&s[i]==s[i-1])
+            {
+                cnt=2;
+                for(int j=1;(i+j)<s.length()&&(i-j-1)>=0;j++)
+                {
+                    if(s[i+j]==s[i-j-1])
+                        cnt+=2;
+                    else
+                        break;
+                }
+                if(cnt>max)
+                {
+                    max=cnt;
+                    result=s.substr(i-cnt/2,cnt);
+                }
+            }
+        }
+        return result;
+    }
 };
