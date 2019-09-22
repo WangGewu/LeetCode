3905a7894bf298efba1e337605301d269b5d85f6
Gewu_Wang <35912578+WangGewu@users.noreply.github.com>
2019-09-22 19:00:17 +0800

    Update 017. Letter Combinations of a Phone Number.cpp

--- LeetCode/017. Letter Combinations of a Phone Number.cpp
+++ LeetCode/017. Letter Combinations of a Phone Number.cpp
@@ -1,26 +1,27 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-        string s[10]{ "","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz" };
-        vector<string>result;
-        int len = digits.length();
-        if (len == 0)
-            return result;
-        string value(len, 'a');//使用value保存每次遍历的结果
-        dfs(result, digits, value, s, 0);
-        return result;
+        vector<string>res;
+        if(digits.length()==0)
+            return res;
+        string s[9]={"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+        string element;
+        generate(element,0,digits,s,res);
+        return res;
     }
-    void dfs(vector<string>&v, string &s1, string &s2, string(&s3)[10], int pos)//s1=digits,s2=value,s3=s
+    void generate(string result,int index,string &digits,string s[9],vector<string>&res)
     {
-        if (pos == s1.length())//到达digits的末尾，存入vector，且返回
+        if(index==digits.length())
         {
-            v.push_back(s2);
+            res.push_back(result);
             return;
         }
-        for (int i = 0; i < s3[s1[pos] - '0'].length(); i++)
+        int num=digits[index]-'0'-2;
+        int len=s[num].length();
+        for(int i=0;i<len;i++)
         {
-            s2[pos] = s3[s1[pos] - '0'][i];
-            dfs(v, s1, s2, s3, pos + 1);
+            string temp=result+s[num][i];
+            generate(temp,index+1,digits,s,res);
         }
     }
 };
