1585093f5ea71e6b83d67cc273af97bb680eec95
Gewu_Wang <35912578+WangGewu@users.noreply.github.com>
2019-11-09 11:19:30 +0800

    Update 033. Search in Rotated Sorted Array.cpp

--- LeetCode/033. Search in Rotated Sorted Array.cpp
+++ LeetCode/033. Search in Rotated Sorted Array.cpp
@@ -25,3 +25,33 @@ public:
         return -1;
     }
 };
+
+
+//二刷
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l=0,r=nums.size()-1;
+        while(l<=r)
+        {
+             int mid=(l+r)/2;
+             if(nums[mid]==target)
+                return mid;
+             if(nums[mid]>=nums[l])
+             {
+                if(target>=nums[l]&&target<nums[mid])
+                    r=mid-1;
+                else
+                    l=mid+1;
+             }
+             else
+             {
+                 if(target>=nums[0]||target<nums[mid])
+                    r=mid-1;
+                else
+                    l=mid+1;
+             }
+        }
+        return -1;
+    }
+};
