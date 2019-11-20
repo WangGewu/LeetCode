10534d25728ed85cd319e3f2cbb0310e4a1cbdab
Gewu_Wang <35912578+WangGewu@users.noreply.github.com>
2019-11-20 11:44:51 +0800

    Update 037. Sudoku Solver.cpp

--- LeetCode/037. Sudoku Solver.cpp
+++ LeetCode/037. Sudoku Solver.cpp
@@ -40,3 +40,57 @@ public:
         return true;
     }
 };
+
+
+//2刷（超时）
+class Solution {
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        int flag=0;
+        solve(board,0,flag);
+    }
+    void solve(vector<vector<char>>& board,int position,int& flag)
+    {
+        if(position==81)
+        {
+            flag=1;
+            return;
+        }
+        if(board[position/9][position%9]!='.')
+            solve(board,position+1,flag);
+        else
+        {
+            for(int i=0;i<9;i++)
+            {
+                char value='0'+i+1;
+                board[position/9][position%9]=value;
+                if(isValidSudoku(board))
+                {
+                    solve(board,position+1,flag);
+                    if(flag==1)
+                        return;
+                }
+                board[position/9][position%9]='.';
+            }
+        }
+    }
+    bool isValidSudoku(vector<vector<char>>& board) {
+        vector<vector<int>>row(9,vector<int>(9,0));
+        vector<vector<int>>column(9,vector<int>(9,0));
+        vector<vector<int>>box(9,vector<int>(9,0));
+        for(int i=0;i<9;i++)
+            for(int j=0;j<9;j++)
+            {
+                if(board[i][j]!='.')
+                {
+                    int num=board[i][j]-'0'-1;
+                    //第几个九宫格
+                    int k=i/3+3*(j/3);
+                    if(row[i][num]||column[j][num]||box[k][num])
+                        return false;
+                    row[i][num]=column[j][num]=box[k][num]=1;
+                }
+            }
+        return true;
+    }
+};
