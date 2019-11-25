7527250a0277c16528a9a97da7d8522ed34a950a
Gewu_Wang <35912578+WangGewu@users.noreply.github.com>
2019-11-25 12:32:15 +0800

    Update 039. Combination Sum.cpp

--- LeetCode/039. Combination Sum.cpp
+++ LeetCode/039. Combination Sum.cpp
@@ -22,3 +22,33 @@ public:
         }
     }
 };
+
+//2刷
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>result;
+        vector<int>current_result;
+        int current_pos=0;
+        solve(result,current_result,candidates,target,current_pos);
+        return result;
+    }
+    void solve(vector<vector<int>>& result,vector<int>&current_result,vector<int>& candidates,int target,int current_pos)
+    {
+        if(target==0)
+        {
+            result.push_back(current_result);
+            return;
+        }
+        else if(target<0)
+            return;
+        else{
+            for(int i=current_pos;i<candidates.size();i++)
+            {
+                current_result.push_back(candidates[i]);
+                solve(result,current_result,candidates,target-candidates[i],i);
+                current_result.pop_back();
+            }
+        }
+    }
+};
