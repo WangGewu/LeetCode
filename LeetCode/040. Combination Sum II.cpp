8218dc6267daa7f4191d3aab36b2e3eba422b855
Gewu_Wang <35912578+WangGewu@users.noreply.github.com>
2019-11-27 21:58:40 +0800

    Update 040. Combination Sum II.cpp

--- LeetCode/040. Combination Sum II.cpp
+++ LeetCode/040. Combination Sum II.cpp
@@ -9,16 +9,52 @@ public:
     }
     void solve(vector<vector<int>>&v, vector<int>&element, int n, int sum, vector<int>&candidates)
     {
-        if (!sum&&find(v.begin(), v.end(), element) == v.end())//±ÜÃâÖØ¸´
+        if (!sum&&find(v.begin(), v.end(), element) == v.end())//é¿å…é‡å¤
         {
             v.push_back(element);
             return;
         }
-        for (int i = n; i < candidates.size() && sum >= candidates[i]; i++)//Ä¿Ç°ÔªËØÖµĞ¡ÓÚµÈÓÚsum£¬¼ÌĞøÑ­»·
+        for (int i = n; i < candidates.size() && sum >= candidates[i]; i++)//ç›®å‰å…ƒç´ å€¼å°äºç­‰äºsumï¼Œç»§ç»­å¾ªç¯
         {
             element.push_back(candidates[i]);
-            solve(v, element, i + 1, sum - candidates[i], candidates);//µİ¹é
-            element.pop_back();//»ØËİ
+            solve(v, element, i + 1, sum - candidates[i], candidates);//é€’å½’
+            element.pop_back();//å›æº¯
+        }
+    }
+};
+
+//2åˆ·
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        sort(candidates.begin(),candidates.end());
+        vector<vector<int>>result;
+        vector<int>current_result;
+        int pos=0;
+        solve(result,candidates,current_result,pos,target);
+        //set<vector<int>> st(result.begin(), result.end());
+        //result.assign(st.begin(), st.end());
+        return result;
+    }
+    void solve(vector<vector<int>>& result,vector<int>candidates,vector<int>current_result,int pos,int target)
+    {
+        int last=-1;
+        if(target==0)
+            {   result.push_back(current_result);
+                return;
+            }
+        else if(target<0)
+        {
+            return ;
+        }
+        for(int i=pos;i<candidates.size();i++)
+        {   
+            if(candidates[i]==last)
+                continue;
+            current_result.push_back(candidates[i]);
+            solve(result,candidates,current_result,i+1,target-candidates[i]);
+            last=candidates[i];
+            current_result.pop_back();
         }
     }
 };
