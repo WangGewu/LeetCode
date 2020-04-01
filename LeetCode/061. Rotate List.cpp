1e065465a165fbca78f2e62bd459989747fed32d
Gewu_Wang <35912578+WangGewu@users.noreply.github.com>
2020-04-01 18:38:28 +0800

    Update 061. Rotate List.cpp

--- LeetCode/061. Rotate List.cpp
+++ LeetCode/061. Rotate List.cpp
@@ -32,3 +32,32 @@ public:
         return head;
     }
 };
+
+//2
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        ListNode* old_head = head,*old_end=NULL;
+        ListNode* p=head;
+        int length=0;
+        while(p)
+        {
+            length++;
+            if(!p->next)
+                old_end=p;
+            p=p->next;
+        }
+        if(length==0||k%length==0)
+            head=old_head;
+        else
+        {
+            p=old_head;
+            for(int i=0;i<length-(k%length)-1;i++)
+                p=p->next;
+            head = p->next;
+            p->next=NULL;
+            old_end->next=old_head;
+        }
+        return head;
+    }
+};
