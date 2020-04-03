fbe5fc8a7c6f29809f125f61166f616fc467612c
Gewu_Wang <35912578+WangGewu@users.noreply.github.com>
2020-04-03 13:52:33 +0800

    Update 064. Minimum Path Sum.cpp

--- LeetCode/064. Minimum Path Sum.cpp
+++ LeetCode/064. Minimum Path Sum.cpp
@@ -19,3 +19,25 @@ public:
         return minDist[m - 1][n - 1];
     }
 };
+//2
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>result(n,vector<int>(m,0));
+        for(int i=0;i<n;i++)
+            for(int j=0;j<m;j++)
+            {
+                if(i==0&&j==0)
+                    result[i][j]=grid[i][j];
+                else if(i==0)
+                    result[i][j]=grid[i][j]+result[i][j-1];
+                else if(j==0)
+                    result[i][j]=grid[i][j]+result[i-1][j];
+                else
+                    result[i][j]=grid[i][j]+min(result[i-1][j],result[i][j-1]);
+            }
+        return result[n-1][m-1];
+    }
+};
