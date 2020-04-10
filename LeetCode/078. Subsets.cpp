eeaea863f8ec50d432f9dd3049b0b842a5a411d2
Gewu_Wang <35912578+WangGewu@users.noreply.github.com>
2020-04-10 13:11:34 +0800

    Update 078. Subsets.cpp

--- LeetCode/078. Subsets.cpp
+++ LeetCode/078. Subsets.cpp
@@ -26,3 +26,30 @@ public:
         }
     }
 };
+
+//2
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>result;
+        vector<int>element;
+        result.push_back(element);
+        int start=0;
+        solve(result,element,nums,start);
+        return result;
+    }
+    void solve(vector<vector<int>>&result,vector<int>&element,vector<int>&nums,int start)
+    {
+        if(element.size()==nums.size())
+            return;
+        for(int i=start;i<nums.size();i++)
+        {
+            element.push_back(nums[i]);
+            result.push_back(element);
+            solve(result,element,nums,i+1);
+            element.pop_back();
+        }
+    }
+};
+
+//3这个题蛮有意思的，可以直接从后遍历，遇到一个数就把所有子集加上该数组成新的子集，遍历完毕即是所有子集————leetcode热评
