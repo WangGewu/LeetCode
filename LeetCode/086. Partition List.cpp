2e3df3e657a25dffc91fe2de524b88419084eda6
Gewu_Wang <35912578+WangGewu@users.noreply.github.com>
2020-04-15 10:54:58 +0800

    Update 086. Partition List.cpp

--- LeetCode/086. Partition List.cpp
+++ LeetCode/086. Partition List.cpp
@@ -25,8 +25,45 @@ public:
             }
             p = p->next;
         }
-        current2->next = nullptr;//将不小于x的链表尾节点next置空
-        current1->next = head2->next;//拼接两个链表
+        current2->next = nullptr;//灏嗕笉灏忎簬x鐨勯摼琛ㄥ熬鑺傜偣next缃┖
+        current1->next = head2->next;//鎷兼帴涓や釜閾捐〃
         return head1->next;
+    }
+};
+
+//2
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode* min_head=new ListNode(-1);
+        ListNode* min_p=min_head;
+        ListNode* max_head=new ListNode(-1);
+        ListNode* max_p=max_head;
+        ListNode* p=head;
+        while(p)
+        {
+            if(p->val<x)
+            {
+                min_p->next=p;
+                min_p=min_p->next;
+            }
+            else
+            {
+                max_p->next=p;
+                max_p=max_p->next;
+            }
+            p=p->next;
+        }
+        max_p->next=NULL;//鍏抽敭
+        min_p->next=max_head->next;
+        return min_head->next; 
     }
 };
