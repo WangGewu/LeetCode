5dee5398052f18b1d9108fa9e975a4585a5d0ebf
Gewu_Wang <35912578+WangGewu@users.noreply.github.com>
2020-04-16 10:38:42 +0800

    Update 088. Merge Sorted Array.cpp

--- LeetCode/088. Merge Sorted Array.cpp
+++ LeetCode/088. Merge Sorted Array.cpp
@@ -30,3 +30,15 @@ public:
         }
     }
 };
+
+//2
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i=m-1,j=n-1,current=m+n-1;
+        while(i>=0&&j>=0)
+            nums1[current--]=nums1[i]>nums2[j]?nums1[i--]:nums2[j--];
+        while(j>=0)//如果nums2没填完。还有一种情况是nums2填完了nums1没填完，这种情况下，nums1就已经有序了，直接返回
+            nums1[current--]=nums2[j--];
+    }   
+};
